3ef29516e7f9fe02a7541603cfc8ac97b28821a8
Will <will@Alexanders-MacBook-Pro.local>
2018-03-06 12:42:31 -0800

    Initial Commit

---
+++ Cheers/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Cheers
+//
+//  Created by Will on 3/6/18.
+//  Copyright © 2018 University of San Diego. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
