23a2f380f5231a07bcc2803552253860f66551a1
Maeve McClatchey <maevemcclatchey@sandiego.edu>
2018-04-11 23:53:41 -0700

    Made Places, DatabaseRecord, and Neighborhood conform to Codable protocol. Added saveToFile and loadFromFile functions.

--- Cheers/Model/Place.swift
+++ Cheers/Model/Place.swift
@@ -8,6 +8,9 @@
 
 import Foundation
 
+let DocumentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+let ArchiveURL = DocumentsDirectory.appendingPathComponent("places").appendingPathExtension("plist")
+
 extension String {
     var lines: [String] {
         var result: [String] = []
@@ -16,13 +19,45 @@ extension String {
     }
 }
 
-public struct Place: Equatable{
+public struct Place: Equatable, Codable{
     var record: DatabaseRecord
     var favorited: Bool
     
     public static func ==(lhs: Place, rhs: Place) -> Bool {
         return lhs.record.id == rhs.record.id
     }
+    
+    enum  CodingKeys: CodingKey{
+        case record
+        case favorited
+    }
+    
+    init(record: DatabaseRecord, favorited: Bool){
+        self.record = record
+        self.favorited = favorited
+    }
+    public init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: Place.CodingKeys.self)
+        record = try values.decode(DatabaseRecord.self, forKey: .record)
+        favorited = try values.decode(Bool.self, forKey: .favorited)
+    }
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: Place.CodingKeys.self)
+        try container.encode(favorited, forKey: .favorited)
+        try container.encode(record, forKey: .record)
+    }
+}
 
+public func saveToFile(favoritedPlaces: Place) {
+    let encoder = PropertyListEncoder()
+    let encodedPlaces = try? encoder.encode(favoritedPlaces)
+    try? encodedPlaces?.write(to: ArchiveURL, options: .noFileProtection)
+}
+
+public func loadFromFile() -> [Place]?  {
+    guard let decodedPlaces = try? Data(contentsOf: ArchiveURL) else {return nil}
+    let decoder = PropertyListDecoder()
+    return try? decoder.decode(Array<Place>.self, from: decodedPlaces
+    )
 }
 
